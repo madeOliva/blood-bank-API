5d5d2f7a984a3eaf9c3a109ae457c5ac79d68aba
Lazaro <lazaromiguelhernandezizquierdo@gmail.com>
2025-06-05 22:49:27 +0200

    addchangestransfusiones

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,8 +26,6 @@ import { MunicipioModule } from './modules/municipio/municipio.module';
 import { PlanTrabajoModule } from './modules/plan_trabajo/plan_trabajo.module';
 import { PedidosDevolucionesModule } from './modules/pedidos_devoluciones/pedidos_devoluciones.module';
 import { HctransfusionesModule } from './modules/hctransfusiones/hctransfusiones.module';
-import { PruebaspostransfusionalesModule } from './modules/pruebaspostransfusionales/pruebaspostransfusionales.module';
-import { PruebaspretransfusionalesModule } from './modules/pruebaspretransfusionales/pruebaspretransfusionales.module';
 import { StockbancohasModule } from './modules/stockbancohas/stockbancohas.module';
 import { TipocomponenteModule } from './modules/tipocomponente/tipocomponente.module';
 import { TipocomponenteespecialModule } from './modules/tipocomponenteespecial/tipocomponenteespecial.module';
@@ -81,8 +79,6 @@ import { HistoriaClinicaModule } from './modules/historia_clinica/historia_clini
     PlanTrabajoModule,
     PreguntaModule,
     ProvinciaModule,
-    PruebaspostransfusionalesModule,
-    PruebaspretransfusionalesModule,
     ReaccionesModule,
     RegistroDonacionModule,
     SalaModule,
--- src/modules/pruebaspretransfusionales/dto/update-pruebaspretransfusionale.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePruebaspretransfusionaleDto } from './create-pruebaspretransfusionale.dto';
-
-export class UpdatePruebaspretransfusionaleDto extends PartialType(CreatePruebaspretransfusionaleDto) {}
